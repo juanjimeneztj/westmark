4863767870a60243f21a2d9525e11454b4deb797
Juan Jimenez <juanjimeneztj@gmail.com>
2021-06-02 10:54:36 -0700

    removed navbar from the header

--- src/index.js
+++ src/index.js
@@ -7,26 +7,6 @@ import './js/ligthslider.js';
     'use strict';
 
     /* 
-        Menu functions
-    */
-    let mobileMenu = document.getElementById('gsweb-mobile-menu');
-    let desktopMenu = document.getElementById('gsweb-desktop-menu');
-    let logoMobile = document.querySelector('.logo-mobile');
-    let closeMobileMenu = document.querySelector('.gsweb-menu-close');
-
-    mobileMenu.addEventListener('click', ()=>{
-        desktopMenu.classList.add('active');
-        logoMobile.classList.add('visible-mobile');
-    });
-    closeMobileMenu.addEventListener('click', ()=>{
-        desktopMenu.classList.remove('active');
-        logoMobile.classList.remove('visible-mobile');
-    });
-    /* 
-        End Menu functions
-    */
-
-    /* 
         Slider functions
     */
     $(document).ready(function() {
